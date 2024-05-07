72f23b1dcbcc7025d752fb8ae839bc7161421ef3
AndersonBillss <anderson@thebillsfamily.com>
2024-05-07 10:14:40 -0600

    pull movie api from backend

--- server/src/routes/movie.routes.ts
+++ server/src/routes/movie.routes.ts
@@ -1,24 +1,43 @@
 //imported modules
-import express from "express"
+import express, { response } from "express"
 import * as dotenv from 'dotenv';
-const movieRouter = express.Router()
 
 //code from other files
 
+//app config
+dotenv.config()
+const movieRouter = express.Router()
+
+//env
+const movieApiKey = process.env.MOVIE_API_KEY
+
+
+const movieApiUrl = 'https://api.themoviedb.org/3/discover/movie?include_adult=false&include_video=false&language=en-US&page=1&sort_by=popularity.desc';
+const movieApiHeaders = new Headers();
+movieApiHeaders.append('accept', 'application/json');
+movieApiHeaders.append('Authorization', `Bearer ${movieApiKey}`);
+
+let movies: any
+fetch(movieApiUrl, {
+    method: 'GET',
+    headers: movieApiHeaders,
+}
+).then(response => {
+if(!response.ok){
+    throw new Error('Api response not OK')
+}
+return response.json()
+}).then((data: any) => {
+    movies = data.results
+}).catch(error => {
+    console.log(error)
+})
 
 
 movieRouter.get("/getMovies", (req, res) => {
-    res.send([
-        {
-            movie: 'a' 
-        },
-        {
-            movie: 'b' 
-        },
-        {
-            movie: 'c' 
-        }
-    ])
+    res.send(movies)
 })
 
 export default movieRouter
+
+
