79f5575ecef4a48ed93b6a3be499c4685256e11c
AndersonBillss <anderson@thebillsfamily.com>
2024-05-08 08:57:23 -0600

    test to see if environments folder is ignored

--- client/src/environments/environment.prod.ts
+++ client/src/environments/environment.prod.ts
@@ -1,3 +1,3 @@
 export const environment: any = {
-    apiUrl: 'my production port',
+    apiUrl: 'Test if this is being ignored by git',
 }
--- client/src/environments/environment.ts
+++ client/src/environments/environment.ts
@@ -1,3 +1,3 @@
 export const environment: any = {
-    apiUrl: 'my development port',
+    apiUrl: 'Test if this is being ignored by git',
 }
