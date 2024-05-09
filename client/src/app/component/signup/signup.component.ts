392949e2dc370cf12755a344491d36c6828c140b
Asher Contreras <asher.contreras1480@stu.mtec.edu>
2024-05-09 10:42:54 -0600

    sign up page linked to firebase auth

--- client/src/app/component/signup/signup.component.ts
+++ client/src/app/component/signup/signup.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { MatCardModule } from '@angular/material/card';
-import {FormControl, Validators, FormsModule, ReactiveFormsModule} from '@angular/forms';
+import {FormControl, Validators, FormsModule, ReactiveFormsModule, FormGroup, FormBuilder} from '@angular/forms';
 import {MatInputModule} from '@angular/material/input';
 import { MatFormFieldModule } from '@angular/material/form-field';
-import {MatButtonModule} from '@angular/material/button';
+import { MatButtonModule } from '@angular/material/button';
+import { AuthService } from '../../auth.service';
 
 @Component({
   selector: 'app-signup',
@@ -13,5 +14,36 @@ import {MatButtonModule} from '@angular/material/button';
   styleUrl: './signup.component.css'
 })
 export class SignupComponent {
+
+    form: FormGroup;
+    fb: FormBuilder = new FormBuilder;
+
+    constructor(private AuthService: AuthService, fb: FormBuilder) {
+        this.form = fb.group({
+            Email: ['', Validators.compose([Validators.required, Validators.email])],
+            Password: ['', Validators.required]
+        })
+     }
+    regiserterUser(Email: string, Password: string) {
+        console.log(Email, Password)
+        this.AuthService.signUp(Email, Password)
+    }
+
+    resetForm() {
+        this.form.reset();
+    }
+
+    onSubmit() {
+        if (this.form.invalid) {
+          console.error('Form is invalid!');
+          return;
+        }
+    
+        const formData = this.form.value;
+        console.log('Form data:', formData);
+        this.regiserterUser(formData.Email, formData.Password)
+    
+        this.resetForm()
+      }
 
 }
