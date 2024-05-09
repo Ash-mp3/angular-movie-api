ff438a739134f984e2be7d812c260396ad08f098
Asher Contreras <asher.contreras1480@stu.mtec.edu>
2024-05-09 08:48:27 -0600

    styles for welcome, login, and signup page added

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,18 +1,17 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./src/**/*.{html,ts}",
-  ],
+  content: ["./src/**/*.{html,ts}"],
   theme: {
     extend: {
       colors: {
-        primary: '#D62828',
-        secondary: '#F77F00',
-        secondaryAccent: '#FCBF49',
-        contrastSecondary:'#EAE2B7',
-        lightBlack:'#EAE2B7',
-      }
+        primary: "#34623F",
+        secondary: "#B39C4D",
+        secondaryAccent: "#EAE2B7",
+        primaryLight: "#768948",
+        primaryDark: "#1E2F23",
+        lightBlack: "#003049",
+      },
     },
   },
   plugins: [],
-}
+};
