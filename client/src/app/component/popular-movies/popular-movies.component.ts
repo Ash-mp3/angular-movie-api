9dd814686816060c0d700aed13b1c880737e4cd2
Adolfo-Contreras <adolfo.alvaradocontreras1760@stu.mtec.edu>
2024-05-10 09:26:18 -0600

    did some style fixes on welcom, navbar, login, signup, and popular-movies components

--- client/src/app/component/popular-movies/popular-movies.component.ts
+++ client/src/app/component/popular-movies/popular-movies.component.ts
@@ -1,41 +1,53 @@
 import { Component, OnInit } from '@angular/core';
-import {MatInputModule} from '@angular/material/input';
-import {MatCardModule} from '@angular/material/card';
-import {MatDividerModule} from '@angular/material/divider';
-import {MatToolbarModule} from '@angular/material/toolbar';
-import {MatIconModule} from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
+import { MatCardModule } from '@angular/material/card';
+import { MatDividerModule } from '@angular/material/divider';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
 import { MatFormFieldModule } from '@angular/material/form-field';
 
 import { MoviesService } from '../../services/movies.service';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
 
-import {NavbarComponent} from '../navbar/navbar.component'
+import { NavbarComponent } from '../navbar/navbar.component';
+import { MatButtonModule } from '@angular/material/button';
+import { MAT_TOOLTIP_DEFAULT_OPTIONS, MatTooltipModule } from '@angular/material/tooltip';
+
+
 @Component({
   selector: 'app-popular-movies',
   standalone: true,
-  imports: [MatInputModule, MatDividerModule, MatCardModule,MatToolbarModule,MatIconModule,MatFormFieldModule,
-    CommonModule, RouterModule,NavbarComponent
+  imports: [
+    MatInputModule,
+    MatDividerModule,
+    MatCardModule,
+    MatToolbarModule,
+    MatIconModule,
+    MatFormFieldModule,
+    CommonModule,
+    RouterModule,
+    NavbarComponent,
+    MatButtonModule,
+    MatTooltipModule,
+  ],
+  providers: [
+    {provide:MAT_TOOLTIP_DEFAULT_OPTIONS, useValue:{showDelay: 750, hideDelay: 750}}
   ],
   templateUrl: './popular-movies.component.html',
-  styleUrl: './popular-movies.component.css'
+  styleUrl: './popular-movies.component.css',
 })
 export class PopularMoviesComponent implements OnInit {
+  movies: any[] = [];
 
-  movies: any[] = []
-  
-  posterUrl = 'https://image.tmdb.org/t/p/w500'
+  posterUrl = 'https://image.tmdb.org/t/p/w500';
 
-  constructor( private moviesService: MoviesService ){
-
-  }
+  constructor(private moviesService: MoviesService) {}
 
   ngOnInit(): void {
-    this.moviesService.getMovies().subscribe(contents => {
-        this.movies = contents
-        console.log(contents)
-      }
-    )
+    this.moviesService.getMovies().subscribe((contents) => {
+      this.movies = contents;
+      console.log(contents);
+    });
   }
-
 }
