87ab32f45ba5ab770d2299618105f2c65dde886c
Asher Contreras <asher.contreras1480@stu.mtec.edu>
2024-05-12 23:03:21 -0600

    majority of styles done for profile/watchlist page

--- client/src/app/component/watchlist/watchlist.component.ts
+++ client/src/app/component/watchlist/watchlist.component.ts
@@ -1,12 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { MatTabsModule } from '@angular/material/tabs';
+import { NavbarComponent } from '../navbar/navbar.component';
+import { MatCardModule } from '@angular/material/card';
+import { MoviesService } from '../../services/movies.service';
+import { RouterModule } from '@angular/router';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-watchlist',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, MatTabsModule, NavbarComponent, MatCardModule, RouterModule],
   templateUrl: './watchlist.component.html',
   styleUrl: './watchlist.component.css'
 })
-export class WatchlistComponent {
+export class WatchlistComponent implements OnInit{
+
+    movies: any[] = [];
+    posterUrl = 'https://image.tmdb.org/t/p/w500';
+
+    constructor(private moviesService: MoviesService) {
+
+    }
+    
+    ngOnInit(): void {
+        this.moviesService.getMovies().subscribe(contents => {
+            this.movies = contents
+          }
+        )
+        console.log(this.movies)
+    }
 
 }
